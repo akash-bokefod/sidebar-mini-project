cba5097d4dca72f0ffc36348aa6372a34a0585f6
akash-bokefod <bokefodakash@gmail.com>
2025-09-06 18:10:23 +0530

    final commit

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
 //feture added
 //addeed button
-<<<<<<< HEAD
 //added navbar
 //added new from
-<<<<<<< HEAD
-=======
 //added new from
 //added photo sec
->>>>>>> feature
-=======
 //added photo sec
 //added live clock
->>>>>>> feature
+
